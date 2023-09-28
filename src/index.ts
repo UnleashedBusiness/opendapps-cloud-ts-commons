657bea10baff0c0b7f30669143d6dc72bd1a7f76
ShadowsCrypto <ceo@opendapps.cloud>
2023-09-28 22:44:58 +0300

    fixed exports of library

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+export * from './web3-services.container'
+export * from './http-services.container'
+
+export * from './web3/enum/organization-type.enum';
+export * from './web3/enum/proposal-state.enum';
+
+export * from './web3/data/token/token.data';
+export * from './web3/data/decentralized-entity/multi-sign-entity.data';
+export * from './web3/data/decentralized-entity/multi-sign-shares-entity.data';
+export * from './web3/data/decentralized-entity/single-owner-entity.data';
+export * from './web3/data/decentralized-entity/decentralized-entity.factory';
+export * from './web3/data/multi-sign-proposal/multi-sign-proposal.data';
+export * from './web3/data/multi-sign-proposal/governance-proposal.factory';
+export * from './web3/data/multi-sign-proposal/multi-sign-shares-proposal.data';
+
 export * from './web3/contract/base/base-deployer.contract';
+export * from './web3/contract/asset-backing.contract';
+export * from './web3/contract/baseline-insurance-deployer.contract';
 export * from './web3/contract/contract-deployer.contract';
 export * from './web3/contract/decentralized-entity-deployer.contract';
 export * from './web3/contract/decentralized-entity-interface.contract';
@@ -7,6 +24,7 @@ export * from './web3/contract/governor-interface.contract';
 export * from './web3/contract/inflation-contract';
 export * from './web3/contract/multi-sign-entity-contract';
 export * from './web3/contract/multi-sign-shares-entity.contract';
+export * from './web3/contract/opendapps-cloud-router.contract';
 export * from './web3/contract/ownership-nft-collection.contract';
 export * from './web3/contract/ownership-shares-nft-collection.contract';
 export * from './web3/contract/referral-engine.contract';
@@ -17,3 +35,10 @@ export * from './web3/contract/token-as-a-service.contract';
 export * from './web3/contract/token-as-a-service-deployer.contract';
 export * from './web3/contract/token-liquidity-treasury.contract';
 export * from './web3/contract/token-rewards-treasury.contract';
+
+export * from './web2/decentralized-entity-http.service'
+export * from './web2/indexing-http.service'
+export * from './web2/deployment.http.service'
+export * from './web2/multi-sign-proposal-http.service'
+export * from './web2/token-as-a-service-deployer-http.service'
+export * from './web2/nft-proxy-http.service'
