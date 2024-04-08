021805a53815581ecf5bd96fb48d204a64e4ee04
ShadowsCrypto <ceo@opendapps.cloud>
2024-04-08 23:53:33 +0300

    feat: new token and pair stats

--- src/web2/stats-http.service.ts
+++ src/web2/stats-http.service.ts
@@ -1,13 +1,53 @@
 import { BaseHttpService } from './base/base-http.service.js';
 import type {StakingStatsDto} from "./data/stats/staking-stats.dto.js";
+import type {TokenStatsDto} from "./data/stats/token-stats.dto.js";
+import type {TokenPairStatsDto} from "./data/stats/token-pair-stats.dto.js";
 
 export class StatsHttpService extends BaseHttpService {
   public static readonly STAKING_STATS_FOR_CHAIN_AND_ADDRESS = '/stats/staking/{targetChain}/{address}';
+  public static readonly TOKEN_LATEST_STATS_FOR_CHAIN_AND_ADDRESS = '/stats/token/{targetChain}/{address}/latest';
+  public static readonly TOKEN_LIST_STATS_FOR_CHAIN_AND_ADDRESS = '/stats/token/{targetChain}/{address}';
+  public static readonly DEX_PAIR_LATEST_STATS_FOR_CHAIN_AND_ADDRESS = '/stats/dex/{targetChain}/{address}/latest';
+  public static readonly DEX_PAIR_LIST_STATS_FOR_CHAIN_AND_ADDRESS = '/stats/dex/{targetChain}/{address}';
 
   public async getStakingStats(chainId: number, address: string): Promise<StakingStatsDto> {
     const relativeUrl = StatsHttpService.STAKING_STATS_FOR_CHAIN_AND_ADDRESS
         .replace(/\{targetChain}/, chainId.toString())
         .replace(/\{address}/, address.toString());
+
+    return this.GET(relativeUrl);
+  }
+
+  public async getLatestTokenStats(chainId: number, address: string): Promise<TokenStatsDto> {
+    const relativeUrl = StatsHttpService.TOKEN_LATEST_STATS_FOR_CHAIN_AND_ADDRESS
+        .replace(/\{targetChain}/, chainId.toString())
+        .replace(/\{address}/, address.toString());
+
+    return this.GET(relativeUrl);
+  }
+
+  public async getLatestDexPairStats(chainId: number, address: string): Promise<TokenPairStatsDto> {
+    const relativeUrl = StatsHttpService.DEX_PAIR_LATEST_STATS_FOR_CHAIN_AND_ADDRESS
+        .replace(/\{targetChain}/, chainId.toString())
+        .replace(/\{address}/, address.toString());
+
+    return this.GET(relativeUrl);
+  }
+
+  public async listTokenStats(chainId: number, address: string, take: number = 10, skip: number = 0): Promise<TokenStatsDto[]> {
+    let relativeUrl = StatsHttpService.TOKEN_LIST_STATS_FOR_CHAIN_AND_ADDRESS
+        .replace(/\{targetChain}/, chainId.toString())
+        .replace(/\{address}/, address.toString());
+    relativeUrl += `?take=${take}&skip=${skip}`;
+
+    return this.GET(relativeUrl);
+  }
+
+  public async listDexPairStats(chainId: number, address: string, take: number = 10, skip: number = 0): Promise<TokenPairStatsDto[]> {
+    let relativeUrl = StatsHttpService.DEX_PAIR_LIST_STATS_FOR_CHAIN_AND_ADDRESS
+        .replace(/\{targetChain}/, chainId.toString())
+        .replace(/\{address}/, address.toString());
+    relativeUrl += `?take=${take}&skip=${skip}`;
 
     return this.GET(relativeUrl);
   }
